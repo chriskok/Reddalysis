c1fcaf11b437512c54611331186447610f24985a
chriskok <pherkok@gmail.com>
2021-02-07 17:02:04 -0600

    Finished testing mongodb

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,27 +2,20 @@ import datetime
 
 from mongoengine import *
 db = connect('mongoengine_test', host='localhost', port=27017)
-db.drop_database('mongoengine_test')
+db.drop_database('mongoengine_test')  # Use only if you want to refresh your database
 
 class User(Document):
     email = StringField(required=True)
-    first_name = StringField(max_length=50, unique=True)
+    # Unique ensures there are no copies in the table with the same value for this field
+    first_name = StringField(max_length=50, unique=True) 
     last_name = StringField(max_length=50)
 
 class Post(Document):
     title = StringField(max_length=120, required=True)
     author = ReferenceField(User)
+    tags = ListField(StringField(max_length=30), default=['hello'])
 
     meta = {'allow_inheritance': True}
-
-class TextPost(Post):
-    content = StringField()
-
-class ImagePost(Post):
-    image_path = StringField()
-
-class LinkPost(Post):
-    link_url = StringField()
 
 user_1 = User(
     email='hulu@hulu.com',
@@ -48,14 +41,14 @@ user_2 = User(
 )
 user_2.save()
 
-post_2 = Post(
-    title='Random Post',
-    author=user_2
-).save()
+# Remember you must save each entry, even after edits
+post_2 = Post(title='Random Post', author=user_2, tags=['testing', 'helps']).save()  
 
 for post in Post.objects:
-    print(post.title)
+    print('Title: {}'.format(post.title))
+    print('Tags: {}'.format(post.tags))
 
+# This is how you query the objects of each table
 user_obj = User.objects(first_name='mr')
 
 for curr_user in user_obj:
