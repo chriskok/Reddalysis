39d5a9bb7df832f356949e6c17b9014bbdad9871
chriskok <pherkok@gmail.com>
2021-02-13 11:00:13 -0600

    Added input parsing test

--- public/data_fetch.js
+++ public/data_fetch.js
@@ -18,3 +18,21 @@ function getRecommendations(animeId, quantity = 5) {
 		`localhost:8000/api/v1/recommendations?anime_code=${animeId}&n_recommendations=${quantity}`
 	);
 }
+
+function parse_input() {
+    console.log(document.getElementById('chosen_sub').value);
+}
+
+
+// Joel's Example
+
+// componentDidMount = async () => {
+//     const animeId = this.props.match.params.searchId;
+//     const referenceAnime = await getAnime(animeId);
+//     const recommendations = await getRecommendations(animeId, 15);
+//     this.setState({ referenceAnime, recommendations });
+// };
+
+// const animeToDisplay = this.state.recommendations[
+//     recCategoryMapping[this.state.recCategory]
+// ];
